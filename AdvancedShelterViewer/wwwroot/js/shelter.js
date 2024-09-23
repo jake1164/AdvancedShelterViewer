2db7c0bb83ba4c0ac928af7d3f47ceda4c79d2fd
Jason Jackson <jake1164@hotmail.com>
2024-09-23 19:28:00 -0400

    Moved methods to js file

---
+++ AdvancedShelterViewer/wwwroot/js/shelter.js
@@ -0,0 +1,36 @@
+﻿"use strict";
+
+const { sjcl, key, iv, fromBits, decrypt, aes, toBits } = require('./aes.js');
+
+window.readFileAsBase64 = (fileInput) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = () => {
+            reader.abort();
+            reject(new Error("Error reading file."));
+        };
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        //reader.readAsDataURL(fileInput.files[0]);
+        reader.readAsText(fileInput.files[0]);
+    });
+};
+
+window.decryptString = (base64String) => {
+    return new Promise((resolve, reject) => {
+        var cipherBits = sjcl.codec.base64.toBits(base64Str);
+        var prp = new sjcl.cipher.aes(key);
+        var plainBits = sjcl.mode.cbc.decrypt(prp, cipherBits, iv);
+        var jsonStr = sjcl.codec.utf8String.fromBits(plainBits);
+        try {
+            console.log(JSON.parse(jsonStr));
+            resolve(JSON.parse(jsonStr));
+        } catch (e) {
+            reject(new Error("Error Decrypting String"));
+        }
+    }
+
+};
+
+export { readFileAsBase64, decryptString };
